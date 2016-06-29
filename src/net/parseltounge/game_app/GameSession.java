fe6d1a96657b45d874201a3763e1671a282f728d
Jack <sharrackor@gmail.com>
2016-06-29 17:07:41 +0200

    Refactor GameSession update() method

--- src/net/parseltounge/game_app/GameSession.java
+++ src/net/parseltounge/game_app/GameSession.java
@@ -15,7 +15,6 @@ import org.w3c.dom.css.Rect;
 
 public class GameSession extends Session {
     //Current session of playing game.
-    //Will have a MenuSession for main menu, PausedSession for game being paused, etc.
 
     private PlayerEntity player_ent;  //Entity controlled by player
     private volatile ArrayList<Entity> ent_array;  //list of all entities
@@ -46,7 +45,7 @@ public class GameSession extends Session {
         }
     }
 
-    public Session next_session() {  //Check if session is over or not.
+    public Session next_session() {
         if(game_over) {
             return next_session;
         } else if(paused) {
@@ -56,100 +55,18 @@ public class GameSession extends Session {
             return null;
     }
 
-    public void update() {  //Update game.
-        //Update strategy is as follows:
-        //1. Check user input and respond.
-        //2. Check for collisions.
-        //3. Update all entities.
-        //4. Check again for all collisions.
-        //5. Add attacks to ent_array and remove old ones.
-        //6. Update camera position.
-        //7. Check if player health lost; if so, terminate session.
-
-        //Handle pausing
-        if(Game.esc_pressed) {
-            Game.esc_pressed = false;  //Prevent rapid swap between pause and game
-            paused = true;  //Invert value of 'paused'
-            //next_session = new PausedSession();  //Will enter paused session once PausedSession exists
-            return;  //Exit update, must pause
+    public void update() {
+        handle_pause();
+        if(!paused) {
+            handle_collisions();
+            update_ents();
+            handle_collisions();
+            handle_attacks();
+            remove_dead_ents();
+            update_camera();
+            handle_player_death();
         }
-
-
-
-        //Loop through entities, check for collisions
-        handle_collisions();
-
-
-
-        //Update all entities
-        for(Entity e : active_ents) {
-            e.update(ent_array);
-        }
-
-
-        //Second collision check after updating positions before rendering, so no visible passing through walls
-        handle_collisions();
-
-
-        //Request new entities from existing entities (eg. attacks)
-        //Append to attacks list first to avoid ConcurrentModificationException
-        ArrayList<AttackEntity> attacks = new ArrayList<AttackEntity>();
-
-        for(Entity e: active_ents) {
-            if(e instanceof LivingEntity && ((LivingEntity) e).get_attack() != null) {  //If living entity and has an attack
-
-                if(!active_ents.contains(((LivingEntity) e).get_attack()))  //If ent_array does not already have attack
-                    attacks.add(((LivingEntity) e).get_attack());  //Get attack from entity and add to attack array
-            }
-        }
-        active_ents.addAll(attacks);  //Attacks are active entities
-        ent_array.addAll(attacks);  //Add all attacks to current entities array
-
-
-        //Remove any old attacks and dead entities
-        //If in ent_array and not in attacks, is old attack, remove
-        //NB: Must add to external arrays first, then remove, to avoid ConcurrentModificationException
-        ArrayList<AttackEntity> old_attacks = new ArrayList<AttackEntity>();
-        ArrayList<LivingEntity> dead_ents = new ArrayList<LivingEntity>();
-
-        for(Entity e: active_ents) {
-            if(e instanceof AttackEntity && !attacks.contains(e)) {
-                old_attacks.add((AttackEntity) e);
-            } else if(e instanceof LivingEntity && ((LivingEntity) e).is_dead()) { //If a dead LivingEntity
-                dead_ents.add((LivingEntity) e);
-            }
-        }
-        ent_array.removeAll(old_attacks);
-        ent_array.removeAll(dead_ents);
-        active_ents.removeAll(old_attacks);
-        active_ents.removeAll(dead_ents);
-
-
-        //Check position of player, adjust camera accordingly
-
-        //If too far right, camera is set so rightmost side of player is 50 pixels right of middle of screen
-        //If too far left, camera is set so left side of player is 50 pixels left of screen middle
-        if(player_ent.get_x() + player_ent.get_hitbox().width - camera_x > Game.G_WIDTH/2 + 50)  //If too far to right
-            camera_x = player_ent.get_x() + player_ent.get_hitbox().width - Game.G_WIDTH/2 - 50;
-        else if(player_ent.get_x() - camera_x < Game.G_WIDTH/2 - 50)  //If too far to left
-            camera_x = player_ent.get_x() - Game.G_WIDTH/2 + 50;
-
-        //If too far down, camera is set so bottom of player is 100 pixels below middle of screen
-        //If too far up, camera is set so top of player is 100 pixels above middle of screen
-        if(player_ent.get_y() + player_ent.get_hitbox().height - camera_y > Game.G_HEIGHT/2 + 100)  //If too far down
-            camera_y = player_ent.get_y() + player_ent.get_hitbox().height - Game.G_HEIGHT/2 - 100;
-        else if(player_ent.get_y() - camera_y < Game.G_HEIGHT/2 - 100)  //If too far up
-            camera_y = player_ent.get_y() - Game.G_HEIGHT/2 + 100;
-
-
-
-        //Check of player died
-        if(!active_ents.contains(player_ent)) {  //Player does not exist, must have died, end game
-            game_over = true;
-            next_session = new GameSession();  //Create new game session (restart)
-            //Will later give a GameOverSession or something similar
-        }
-    } //end of update()
+    }
 
     public void render(BufferStrategy strategy) {  //Render next frame
         Graphics g = strategy.getDrawGraphics();  //Get graphics context to render on
@@ -176,25 +93,89 @@ public class GameSession extends Session {
         g.dispose();
     }
 
-    void handle_collisions() {  //Handle all collisions.
+    private void handle_pause() {
+        if(Game.esc_pressed) {
+            Game.esc_pressed = false;  //Prevent rapid swap between pause and game
+            paused = true;  //Invert value of 'paused'
+            //next_session = new PausedSession();  //Will enter paused session once PausedSession exists
+        }
+    }
 
-        //Technique: check for collisions that will happen, then solve.
-        //1. Check where all entities will be on next update. Find which tiles they will occupy.
-        //2. Find minimum between distance entities want to travel and where they can go without intersecting with hitboxes.
-
+    private void handle_collisions() {
+        // Check for collisions that will happen next update
         for(Entity e : active_ents) {  //Only active entities can collide
-
-            //Predict positions of entity. Check for future collisions.
             Hitbox hbox_temp = new Hitbox(e.hitbox.x + e.get_dx(), e.hitbox.y + e.get_dy(), e.hitbox.width, e.hitbox.height);
-
-            //Check if will collide with anything
             for(Entity other : ent_array) {
-                if(other.hitbox.intersects(hbox_temp) && e != other) {  //If collides, add to list of collisions
+                if(other.hitbox.intersects(hbox_temp) && e != other) {
                     e.collision(other);
                 }
-            } //for other loop
+            }
+        }
+    }
 
-        } //for e loop
+    private void update_ents() {
+        for(Entity e : active_ents) {
+            e.update(ent_array);
+        }
+    }
 
+    private void handle_attacks() {
+        //Request new attack entities from existing entities
+        //Append to attacks list first to avoid ConcurrentModificationException
+        ArrayList<AttackEntity> attacks = new ArrayList<AttackEntity>();
+        for(Entity e: active_ents) {
+            if(e instanceof LivingEntity && ((LivingEntity) e).get_attack() != null) {
+                if(!active_ents.contains(((LivingEntity) e).get_attack()))  //If ent_array does not already have attack
+                    attacks.add(((LivingEntity) e).get_attack());  //Get attack from entity and add to attack array
+            }
+        }
+        active_ents.addAll(attacks);  //Attacks are active entities
+        ent_array.addAll(attacks);  //Add all attacks to current entities array
+
+        //Remove any old attacks
+        //NB: Must add to external arrays first, then remove, to avoid ConcurrentModificationException
+        ArrayList<AttackEntity> old_attacks = new ArrayList<AttackEntity>();
+        for(Entity e: active_ents) {
+            if(e instanceof AttackEntity && !attacks.contains(e)) {
+                old_attacks.add((AttackEntity) e);
+            }
+        }
+        ent_array.removeAll(old_attacks);
+        active_ents.removeAll(old_attacks);
+    }
+
+    private void remove_dead_ents() {
+        ArrayList<LivingEntity> dead_ents = new ArrayList<LivingEntity>();
+        for(Entity e: active_ents) {
+            if(e instanceof LivingEntity && ((LivingEntity) e).is_dead()) { //If a dead LivingEntity
+                dead_ents.add((LivingEntity) e);
+            }
+        }
+        ent_array.removeAll(dead_ents);
+        active_ents.removeAll(dead_ents);
+    }
+
+    private void update_camera() {
+        //If too far right, camera is set so rightmost side of player is 50 pixels right of middle of screen
+        //If too far left, camera is set so left side of player is 50 pixels left of screen middle
+        if(player_ent.get_x() + player_ent.get_hitbox().width - camera_x > Game.G_WIDTH/2 + 50)  //If too far to right
+            camera_x = player_ent.get_x() + player_ent.get_hitbox().width - Game.G_WIDTH/2 - 50;
+        else if(player_ent.get_x() - camera_x < Game.G_WIDTH/2 - 50)  //If too far to left
+            camera_x = player_ent.get_x() - Game.G_WIDTH/2 + 50;
+
+        //If too far down, camera is set so bottom of player is 100 pixels below middle of screen
+        //If too far up, camera is set so top of player is 100 pixels above middle of screen
+        if(player_ent.get_y() + player_ent.get_hitbox().height - camera_y > Game.G_HEIGHT/2 + 100)  //If too far down
+            camera_y = player_ent.get_y() + player_ent.get_hitbox().height - Game.G_HEIGHT/2 - 100;
+        else if(player_ent.get_y() - camera_y < Game.G_HEIGHT/2 - 100)  //If too far up
+            camera_y = player_ent.get_y() - Game.G_HEIGHT/2 + 100;
+    }
+
+    private void handle_player_death() {
+        if(!active_ents.contains(player_ent)) {  //Player does not exist, must have died, end game
+            game_over = true;
+            next_session = new GameSession();  //Create new game session (restart)
+            //Will later give a GameOverSession or something similar
+        }
     }
 }
